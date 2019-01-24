b17ee1a852477abc547cba914e0b55b2bfd90ef9
Felix <f.samplexyt@gmail.com>
2019-01-24 13:15:25 +0100

    Updates

---
+++ precache-manifest.3e4a993ca43ba91b747aefc98f760233.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fa4ddc155dc69224764",
+    "url": "/react-todo/static/css/main.63b557bf.chunk.css"
+  },
+  {
+    "revision": "3fa4ddc155dc69224764",
+    "url": "/react-todo/static/js/main.3fa4ddc1.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/react-todo/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "004a237c649775e9fcf8bafbbd9138a1",
+    "url": "/react-todo/index.html"
+  }
+];
